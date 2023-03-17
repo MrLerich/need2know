62f2ed0d7efc5e187e74b052466df67183770270
MrLerich <93153582+MrLerich@users.noreply.github.com>
2023-03-17 15:31:15 +0300

    lst2dict

--- dct_tricks/create_from_2lst.py
+++ dct_tricks/create_from_2lst.py
@@ -1,18 +1,44 @@
-# Создание словаря из списков
+# # Создание словаря из списков
+#
+# street = ['a', 'b', 'c', 'd', 'a']
+# house_number = [1, 2, 3, 4, 5]
+#
+# def create_dct(lst1: list, lst2: list):
+#     for i in range(len(house_number)):
+#         new_dct = dict(zip(lst1, lst2))
+#     print(new_dct)
+#
+# ## Этот метод вызовет ошибку при разном количестве элементов в списках
+# # for i in range(len(house_number)):
+# #     c[street[i]] = house_number[i]
+# # print(c)
+#
+# if __name__ == '__main__':
+#     create_dct(street, house_number)
+#
+#
+#
+from collections import defaultdict
 
-street = ['a', 'b', 'c', 'd']
-house_number = [1, 2, 3, 4]
+street: list = ['a', 'b', 'c', 'd', 'a']
+house_number: list = [1, 2, 3, 4, 5]
 
-def create_dct(lst1: list, lst2: list):
-    for i in range(len(house_number)):
-        new_dct = dict(zip(lst1, lst2))
-    print(new_dct)
+data_after_1st_step: list = list(zip(street, house_number))
+print(f'data_after_1st_step: {data_after_1st_step}')
 
-## Этот метод вызовет оошибку при разном количестве элементов в списках
-# for i in range(len(house_number)):
-#     c[street[i]] = house_number[i]
-# print(c)
 
-if __name__ == '__main__':
-    create_dct(street, house_number)
+## ver1
+# only_with_unique_street: dict = defaultdict(list)
+# for i, j in data_after_1st_step:
+#     only_with_unique_street[i].append(j)
+#
 
+# # https://www.geeksforgeeks.org/python-convert-list-of-tuples-to-dictionary-value-lists/
+# print(f'only_with_unique_street: {str(dict(only_with_unique_street))}')
+
+# Ver2 - мне он больше нра
+only_with_unique_street = {}
+for k, v in data_after_1st_step:
+    only_with_unique_street.setdefault(k, []).append(v)
+# # дальше времени не хватает - опять за руль :(
+print(str(only_with_unique_street))
